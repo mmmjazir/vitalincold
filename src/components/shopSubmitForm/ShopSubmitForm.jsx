7ac0d5048f313e2d4178a65b65b85b31b483035a
Jazir mk <64349622+mmmjazir@users.noreply.github.com>
2023-09-08 11:59:18 +0530

    Update ShopSubmitForm.jsx

--- src/components/shopSubmitForm/ShopSubmitForm.jsx
+++ src/components/shopSubmitForm/ShopSubmitForm.jsx
@@ -71,15 +71,39 @@ const ShopSubmitForm = ({setIsLoading}) => {
                value={shopname}   
                className={emptyFields.includes('shopname') ? 'error' : ''}
             />
-            <label>Shop Address:</label>
-            <input 
-                type="text" 
-                value={address} 
-                onChange={(e)=>{
-                   setAddress(e.target.value)    
-            }} 
-            className={emptyFields.includes('address') ? 'error' : ''}
-            />
+             <label>Located State:</label>
+            <select className={emptyFields.includes('address') ? 'error' : ''} value={address} onChange={(e)=>{
+                   setAddress(e.target.value)   
+            }
+            }>
+          <option value="">All States</option>
+          <option value="andhrapradesh">Andhra Pradesh</option>
+          <option value="arunachalpradesh">Arunachal Pradesh</option>
+          <option value="assam">Assam</option>
+          <option value="bihar">Bihar</option>
+          <option value="chhattisgarh">Chhattisgarh</option>
+          <option value="goa">Goa</option>
+          <option value="gujarat">Gujarat</option>
+          <option value="haryana">Haryana</option>
+          <option value="himachalpradesh">Himachal Pradesh</option>
+          <option value="jammu&kashmir">Jammu & Kashmir</option>
+          <option value="jharkhand">Jharkhand</option>
+          <option value="karnataka">Karnataka</option>
+          <option value="kerala">Kerala</option>
+          <option value="madhyapradesh">Madhya Pradesh</option>
+          <option value="maharashtra">Maharashtra</option>
+          <option value="manipur">Manipur</option>
+          <option value="meghalaya">Meghalaya</option>
+          <option value="mizoram">Mizoram</option>
+          <option value="nagaland">Nagaland</option>
+          <option value="odisha">Odisha</option>
+          <option value="punjab">Punjab</option>
+          <option value="rajasthan">Rajasthan</option>
+          <option value="sikkim">Sikkim</option>
+          <option value="tamilnadu">Tamil Nadu	</option>
+          <option value="telangana">Telangana</option>
+          <option value="tripura">Tripura</option>
+        </select>
 
             <button>Add Shop</button>
             {successMessage &&  <div className="success">{successMessage}</div> }
